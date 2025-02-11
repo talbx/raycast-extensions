0b6efb9db15a143477275dce0e0cdd2a889eb532
talbx <dev@talbx.de>
2025-02-11 10:24:22 +0100

    fix: lint

--- extensions/logic-projects/src/find-projects.ts
+++ extensions/logic-projects/src/find-projects.ts
@@ -1,45 +1,45 @@
 import crypto from "node:crypto";
-import {LogicProject} from "./recent-logic-pro-projects";
-import {promisify} from "node:util";
-import {exec} from "node:child_process";
-import {getPreferenceValues} from "@raycast/api";
+import { LogicProject } from "./recent-logic-pro-projects";
+import { promisify } from "node:util";
+import { exec } from "node:child_process";
+import { getPreferenceValues } from "@raycast/api";
 
 export const findProjects = (): Promise<LogicProject[]> => {
-    const location = getPreferenceValues<Preferences>()["projects-location"];
-    const execAsync = promisify(exec);
-    return execAsync(
-        // `ls -A1 ${path} | grep .logicx`
-        `find ${location} -regex ".*\\.\\(logicx\\)" | head -n 200 | xargs -I{} stat -f "%N,%m" "{}"`,
-    )
-        .then((result) => {
-            if (result.stderr !== null && result.stderr !== "") {
-                return [];
-            }
-            const projects: LogicProject[] = result.stdout
-                .trim()
-                .split("\n")
-                .filter((entry) => entry.includes(","))
-                .map((entry) => {
-                    const [p, lastModified] = entry.split(",");
-                    const lastModifiedDate = new Date(Number(lastModified) * 1000);
-                    const lastSlashIndex = p.lastIndexOf("/");
-                    const afterLastSlash = p.substring(lastSlashIndex + 1);
-                    return {
-                        name: afterLastSlash,
-                        lastModified: isNaN(lastModifiedDate.getTime()) ? new Date(0) : lastModifiedDate, // Handle invalid dates
-                        id: crypto.createHash("md5").update(entry).digest("hex"),
-                        path: p,
-                    };
-                })
-                .sort((a, b) => {
-                    if (a.lastModified < b.lastModified) {
-                        return 1;
-                    }
-                    return -1;
-                });
-            return projects
+  const location = getPreferenceValues<Preferences>()["projects-location"];
+  const execAsync = promisify(exec);
+  return execAsync(
+    // `ls -A1 ${path} | grep .logicx`
+    `find ${location} -regex ".*\\.\\(logicx\\)" | head -n 200 | xargs -I{} stat -f "%N,%m" "{}"`,
+  )
+    .then((result) => {
+      if (result.stderr !== null && result.stderr !== "") {
+        return [];
+      }
+      const projects: LogicProject[] = result.stdout
+        .trim()
+        .split("\n")
+        .filter((entry) => entry.includes(","))
+        .map((entry) => {
+          const [p, lastModified] = entry.split(",");
+          const lastModifiedDate = new Date(Number(lastModified) * 1000);
+          const lastSlashIndex = p.lastIndexOf("/");
+          const afterLastSlash = p.substring(lastSlashIndex + 1);
+          return {
+            name: afterLastSlash,
+            lastModified: isNaN(lastModifiedDate.getTime()) ? new Date(0) : lastModifiedDate, // Handle invalid dates
+            id: crypto.createHash("md5").update(entry).digest("hex"),
+            path: p,
+          };
         })
-        .catch(() => {
-            return [];
+        .sort((a, b) => {
+          if (a.lastModified < b.lastModified) {
+            return 1;
+          }
+          return -1;
         });
-}
+      return projects;
+    })
+    .catch(() => {
+      return [];
+    });
+};
--- extensions/logic-projects/src/recent-logic-pro-projects.tsx
+++ extensions/logic-projects/src/recent-logic-pro-projects.tsx
@@ -1,123 +1,120 @@
 import {
-    Action,
-    ActionPanel,
-    getApplications,
-    getPreferenceValues,
-    Icon,
-    List,
-    open,
-    openCommandPreferences,
-    showToast,
-    Toast,
+  Action,
+  ActionPanel,
+  getApplications,
+  getPreferenceValues,
+  Icon,
+  List,
+  open,
+  openCommandPreferences,
+  showToast,
+  Toast,
 } from "@raycast/api";
-import {useEffect} from "react";
-import {showFailureToast, useCachedPromise} from "@raycast/utils";
-import {findProjects} from "./find-projects";
+import { useEffect } from "react";
+import { showFailureToast, useCachedPromise } from "@raycast/utils";
+import { findProjects } from "./find-projects";
 
 export interface LogicProject {
-    name: string;
-    id: string;
-    path: string;
-    lastModified: Date;
+  name: string;
+  id: string;
+  path: string;
+  lastModified: Date;
 }
 
 const isLogicInstalled = async () => {
-    const apps = await getApplications();
-    return apps.some(({bundleId}) => bundleId === "com.apple.logic10");
+  const apps = await getApplications();
+  return apps.some(({ bundleId }) => bundleId === "com.apple.logic10");
 };
 
 export default function Command() {
-    const preferences = getPreferenceValues<Preferences>();
+  const preferences = getPreferenceValues<Preferences>();
 
-    isLogicInstalled().then((installed) => {
-        if (!installed) {
-            const options: Toast.Options = {
-                style: Toast.Style.Failure,
-                title: "Logic Pro is not installed.",
-                message: "Install it from: https://www.apple.com/logic-pro/",
-                primaryAction: {
-                    title: "Go to https://www.apple.com/logic-pro/",
-                    onAction: (toast) => {
-                        open("https://www.apple.com/logic-pro/").then(() => toast.hide());
-                    },
+  isLogicInstalled().then((installed) => {
+    if (!installed) {
+      const options: Toast.Options = {
+        style: Toast.Style.Failure,
+        title: "Logic Pro is not installed.",
+        message: "Install it from: https://www.apple.com/logic-pro/",
+        primaryAction: {
+          title: "Go to https://www.apple.com/logic-pro/",
+          onAction: (toast) => {
+            open("https://www.apple.com/logic-pro/").then(() => toast.hide());
+          },
+        },
+      };
+      return showToast(options);
+    }
+  });
+
+  const { isLoading, data, error } = useCachedPromise(async () => await findProjects(), [], { keepPreviousData: true });
+
+  useEffect(() => {
+    if (error !== undefined) {
+      showFailureToast(error, {
+        title: "Something went wrong",
+      });
+    }
+  }, [error]);
+
+  return (
+    <List
+      isLoading={isLoading}
+      filtering={true}
+      throttle={true}
+      searchBarPlaceholder={"Filter projects...yy"}
+      actions={
+        <ActionPanel>
+          <Action title="Open Extension Preferences" onAction={openCommandPreferences} icon={Icon.WrenchScrewdriver} />
+        </ActionPanel>
+      }
+    >
+      {(data || []).map((project) => {
+        return (
+          <List.Item
+            key={project.id}
+            accessories={[
+              {
+                date: {
+                  value: project.lastModified,
                 },
-            };
-            return showToast(options);
-        }
-    });
-
-    const {isLoading, data, error} = useCachedPromise(
-        async () => await findProjects(), [], {keepPreviousData: true});
-
-
-    useEffect(() => {
-        if (error !== undefined) {
-            showFailureToast(error, {
-                title: "Something went wrong",
-            });
-        }
-    }, [error]);
-
-    return (
-        <List
-            isLoading={isLoading}
-            filtering={true}
-            throttle={true}
-            searchBarPlaceholder={"Filter projects...yy"}
+              },
+            ]}
+            icon={{
+              source:
+                "https://help.apple.com/assets/654E7F8CD472768668095520/654E7F9560B6B45E960FE823/de_DE/390711ce08c61bf054d3dc4dfb9080ae.png",
+            }}
+            title={project.name}
             actions={
-                <ActionPanel>
-                    <Action title="Open Extension Preferences" onAction={openCommandPreferences}
-                            icon={Icon.WrenchScrewdriver}/>
-                </ActionPanel>
+              <ActionPanel>
+                <Action title="Open Project" onAction={() => open(project.path)} icon={Icon.Play} />
+                <Action
+                  title="Open Extension Preferences"
+                  onAction={openCommandPreferences}
+                  icon={Icon.WrenchScrewdriver}
+                />
+              </ActionPanel>
             }
-        >
-            {(data || []).map((project) => {
-                return (
-                    <List.Item
-                        key={project.id}
-                        accessories={[
-                            {
-                                date: {
-                                    value: project.lastModified,
-                                },
-                            },
-                        ]}
-                        icon={{
-                            source:
-                                "https://help.apple.com/assets/654E7F8CD472768668095520/654E7F9560B6B45E960FE823/de_DE/390711ce08c61bf054d3dc4dfb9080ae.png",
-                        }}
-                        title={project.name}
-                        actions={
-                            <ActionPanel>
-                                <Action title="Open Project" onAction={() => open(project.path)} icon={Icon.Play}/>
-                                <Action
-                                    title="Open Extension Preferences"
-                                    onAction={openCommandPreferences}
-                                    icon={Icon.WrenchScrewdriver}
-                                />
-                            </ActionPanel>
-                        }
-                    />
-                );
-            })}
-            {(data || []).length == 0 && (
-                <List.EmptyView
-                    actions={
-                        <ActionPanel>
-                            <Action
-                                title="Open Extension Preferences"
-                                onAction={openCommandPreferences}
-                                icon={Icon.WrenchScrewdriver}
-                            />
-                        </ActionPanel>
-                    }
-                    icon={Icon.EmojiSad}
-                    title={"No Logic projects found."}
-                    description={`Are you sure you picked the right directory?
+          />
+        );
+      })}
+      {(data || []).length == 0 && (
+        <List.EmptyView
+          actions={
+            <ActionPanel>
+              <Action
+                title="Open Extension Preferences"
+                onAction={openCommandPreferences}
+                icon={Icon.WrenchScrewdriver}
+              />
+            </ActionPanel>
+          }
+          icon={Icon.EmojiSad}
+          title={"No Logic projects found."}
+          description={`Are you sure you picked the right directory?
 ${preferences["projects-location"]} is currently selected.
 Open the extension preferences (Enter) to change the directory.`}
-                ></List.EmptyView>
-            )}
-        </List>
-    );
+        ></List.EmptyView>
+      )}
+    </List>
+  );
 }
